0eac973f657758f642569da78ecdc51e30d21a8e
Shimran Rodeet <srozbu1@toromail.csudh.edu>
2025-10-30 20:27:50 +0600

    initial commit created the layouts

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+
+function App() {
+
+  return (
+    <>
+    <div className="main">
+
+    <div className="sectionLeftSide">
+
+      <h1>Books Dashboard</h1>
+      <span className="item">Dashboard</span>
+      <span className="item">Search</span>
+      <span className="item">About</span>
+
+    </div>
+
+    <div className="sectionRightSide">
+
+      <div className="sectionAttributes">
+
+       <div className="attibutes">
+       
+       </div>
+
+        <div className="attibutes">
+       
+       </div>
+
+       <div className="attibutes">
+       
+       </div>
+
+      </div>
+
+      <div className="sectionMainList">
+        <div className="searchOptions">
+        <input type="text" id="myTextInput" name="myTextInput" placeholder="Enter your text here"></input>
+        <input type="range" min="1" max="100" id="myRange"></input>
+        <button>Search</button>
+        </div>
+
+      </div>
+
+    </div>
+
+
+    </div>  
+    </>
+  )
+}
+
+export default App
